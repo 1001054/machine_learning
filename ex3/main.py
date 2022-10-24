c00074d3cf030cece67431d84de858ca3ad86e77
1001054 <c1001054@newcastle.ac.uk>
2022-10-24 10:45:27 +0800

    show 100 random images at 24/10/2022

--- ex3/main.py
+++ ex3/main.py
@@ -1,14 +1,17 @@
-# instruction
-# For this exercise, you will use logistic regression and neural networks to
-# recognize handwritten digits (from 0 to 9).
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+
+# instruction
+# For this exercise, you will use logistic regression and neural networks to
+# recognize handwritten digits (from 0 to 9).
 
 def get_hundred(X):
     res = []
     for i in range(100):
-        res.append(X[i])
+        r = random.randint(0, 5000)
+        res.append(X[r])
     return res
 
 def generate_image(samples):
@@ -18,10 +21,22 @@ def generate_image(samples):
     for i in range(10):
         temp = []
         for j in range(10):
-            temp.append(np.reshape(samples[a], (20, 20)))
+            # transfer the one row into two dimension image
+            image = np.reshape(samples[a], (20, 20))
+            # swap the row and the collom
+            image = [[row[i] for row in image] for i in range(len(image[0]))]
+            temp.append(image)
             a += 1
         res.append(temp)
     return res
+
+def show_images(images):
+    for i in range(10):
+        for j in range(10):
+            plt.subplot(10, 10, i * 10 + j + 1)
+            plt.imshow(images[i][j], cmap="Greys_r")
+            plt.xticks([])
+            plt.yticks([])
 
 # load the all the data from the file
 file = loadmat("ex3data1.mat")
@@ -34,10 +49,5 @@ samples = get_hundred(X)
 # transfer the data into the image matrix
 images = generate_image(samples)
 # show the images
-for i in range(10):
-    for j in range(10):
-        plt.subplot(10, 10, i * 10 + j + 1)
-        plt.imshow(images[i][j], cmap="Greys_r")
-        plt.xticks([])
-        plt.yticks([])
+show_images(images)
 plt.show()
